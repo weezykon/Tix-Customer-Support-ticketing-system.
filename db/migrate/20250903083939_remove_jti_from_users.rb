0c010c1806339e5fa56d1bd4f89835180df94a82
Akinbode Akinbode <weezykon@gmail.com>
2025-09-07 10:35:03 +0100

    Specify column type when removing jti from users table

--- db/migrate/20250903083939_remove_jti_from_users.rb
+++ db/migrate/20250903083939_remove_jti_from_users.rb
@@ -3,6 +3,6 @@
 class RemoveJtiFromUsers < ActiveRecord::Migration[7.0]
   def change
     remove_index :users, :jti
-    remove_column :users, :jti
+    remove_column :users, :jti, :string
   end
 end
