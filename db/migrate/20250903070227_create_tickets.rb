0aeaed33bea063d05f3a91869ebb9be5b4d33edc
Akinbode Akinbode <weezykon@gmail.com>
2025-09-07 10:34:47 +0100

    Migrate tickets table to use UUID primary keys and foreign keys

--- db/migrate/20250903070227_create_tickets.rb
+++ db/migrate/20250903070227_create_tickets.rb
@@ -2,11 +2,11 @@
 
 class CreateTickets < ActiveRecord::Migration[7.0]
   def change
-    create_table :tickets do |t|
+    create_table :tickets, id: :uuid do |t|
       t.string :title
       t.text :description
       t.string :status
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true, type: :uuid
 
       t.timestamps
     end
