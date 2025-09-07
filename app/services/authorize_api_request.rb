731befe47183c83319bc985a31b7a1cbc761ae68
Akinbode Akinbode <weezykon@gmail.com>
2025-09-07 10:34:24 +0100

    Refactor AuthorizeApiRequest to prepend SimpleCommand and add debug logging

--- app/services/authorize_api_request.rb
+++ app/services/authorize_api_request.rb
@@ -2,7 +2,7 @@
 
 # Authorizes API requests by validating JWT tokens.
 class AuthorizeApiRequest
-  include SimpleCommand
+  prepend SimpleCommand
 
   def initialize(headers = {})
     @headers = headers
@@ -19,15 +19,21 @@ class AuthorizeApiRequest
   attr_reader :headers
 
   def user
-    @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
+    user_id = decoded_auth_token[:user_id] if decoded_auth_token
+    Rails.logger.debug "User ID from token: #{user_id}"
+    @user ||= User.find(user_id) if user_id
+    Rails.logger.debug "User found: #{@user.present?}"
     @user || errors.add(:token, 'Invalid token') && nil
   end
 
   def decoded_auth_token
-    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+    token = http_auth_header
+    Rails.logger.debug "Token extracted: #{token}"
+    @decoded_auth_token ||= JsonWebToken.decode(token)
   end
 
   def http_auth_header
+    Rails.logger.debug "Authorization header: #{headers['Authorization']}"
     return headers['Authorization'].split(' ').last if headers['Authorization'].present?
 
     errors.add(:token, 'Missing token')
