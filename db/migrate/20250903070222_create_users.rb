aa14438fd6df7c35d36e432a48c0fc1e63871358
Akinbode Akinbode <weezykon@gmail.com>
2025-09-07 10:34:39 +0100

    Migrate users table to use UUID primary keys

--- db/migrate/20250903070222_create_users.rb
+++ db/migrate/20250903070222_create_users.rb
@@ -2,7 +2,7 @@
 
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |t|
+    create_table :users, id: :uuid do |t|
       t.string :name
       t.string :email
       t.string :role
