bc295974eda94d937a99f9c7f6b792b191ed4c81
Akinbode Akinbode <weezykon@gmail.com>
2025-09-07 11:02:07 +0100

    Add priority argument to createTicket mutation

--- app/graphql/mutations/create_ticket.rb
+++ app/graphql/mutations/create_ticket.rb
@@ -5,14 +5,15 @@ module Mutations
   class CreateTicket < BaseMutation
     argument :title, String, required: true
     argument :description, String, required: true
+    argument :priority, String, required: true
 
     field :ticket, Types::TicketType, null: false
     field :errors, [String], null: false
 
-    def resolve(title:, description:)
+    def resolve(title:, description:, priority:)
       raise GraphQL::ExecutionError, 'Authentication required' unless context[:current_user]
 
-      ticket = Ticket.new(title: title, description: description, user: context[:current_user], status: 'open')
+      ticket = Ticket.new(title: title, description: description, priority: priority, user: context[:current_user], status: 'open')
       if ticket.save
         { ticket: ticket, errors: [] }
       else
