25dd445ae9e1d65260a9b1b0bcd4f7b8f922dc85
Akinbode Akinbode <weezykon@gmail.com>
2025-09-07 10:34:55 +0100

    Migrate comments table to use UUID primary keys and foreign keys

--- db/migrate/20250903070232_create_comments.rb
+++ db/migrate/20250903070232_create_comments.rb
@@ -2,10 +2,10 @@
 
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
-    create_table :comments do |t|
+    create_table :comments, id: :uuid do |t|
       t.text :content
-      t.references :ticket, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :ticket, null: false, foreign_key: true, type: :uuid
+      t.references :user, null: false, foreign_key: true, type: :uuid
 
       t.timestamps
     end
